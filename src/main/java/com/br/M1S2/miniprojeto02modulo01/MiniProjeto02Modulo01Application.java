2ba59f50c397854c73f2d6f2fbac532a3b54d27e
David Dutra <dadutra@hotmail.com>
2024-04-04 20:22:11 -0300

    Iniciando a branche main

---
+++ src/main/java/com/br/M1S2/miniprojeto02modulo01/MiniProjeto02Modulo01Application.java
@@ -0,0 +1,15 @@
+package com.br.M1S2.miniprojeto02modulo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjeto02Modulo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjeto02Modulo01Application.class, args);
+
+		System.out.println("Teste");
+	}
+
+}
---
+++ src/test/java/com/br/M1S2/miniprojeto02modulo01/MiniProjeto02Modulo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.M1S2.miniprojeto02modulo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjeto02Modulo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
