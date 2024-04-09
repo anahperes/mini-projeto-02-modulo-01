a6fe0bdd6e7236ef93b497f35d099eb22dfdc124
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-09 16:41:34 -0300

    Update NotaController.java

--- src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/NotaController.java
+++ src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/NotaController.java
@@ -1,14 +1,38 @@
 package com.br.M1S2.miniprojeto02modulo01.controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.br.M1S2.miniprojeto02modulo01.entity.NotaEntity;
+import com.br.M1S2.miniprojeto02modulo01.service.NotaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("nota")
-
+@AllArgsConstructor
+@RequestMapping("notas")
 public class NotaController {
 
+    private final NotaService notaService;
 
+    @PostMapping
+    public ResponseEntity<NotaEntity> adicionarNota(@RequestBody NotaEntity notaRequest) {
+        NotaEntity novaNota = notaService.adicionarNota(notaRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaNota);
+    }
+
+    @GetMapping("/matriculas/{id}")
+    public ResponseEntity<List<NotaEntity>> listarNotas(@PathVariable Long id) {
+        List<NotaEntity> notas = notaService.notasPorMatricula(id);
+        return ResponseEntity.status(HttpStatus.OK).body(notas);
+    }
+
+    // Em NotaController.java
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> excluirNota(@PathVariable Long id) {
+        notaService.excluirNotaPorId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
