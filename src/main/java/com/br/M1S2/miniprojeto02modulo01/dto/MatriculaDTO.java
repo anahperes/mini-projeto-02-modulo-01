7ec50c68fa7dcf6d272a8a099146faffe7217480
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-09 04:28:13 -0300

    Create DTO - Matricula

---
+++ src/main/java/com/br/M1S2/miniprojeto02modulo01/dto/MatriculaDTO.java
@@ -0,0 +1,16 @@
+package com.br.M1S2.miniprojeto02modulo01.dto;
+
+public class MatriculaDTO {
+    private Long idAluno;
+    private Long idDisciplina;
+
+    // getters e setters para idAluno
+    public Long getIdAluno() {
+        return idAluno;
+    }
+
+    // getters e setters para idDisciplina
+    public Long getIdDisciplina() {
+        return idDisciplina;
+    }
+}
