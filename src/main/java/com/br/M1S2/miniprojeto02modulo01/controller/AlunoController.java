4bc9cc69b8ce71b0db50070db80cb96633abb7f7
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-04-11 23:02:02 -0300

    adicionados logs debug em alunoController

--- src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/AlunoController.java
+++ src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/AlunoController.java
@@ -1,45 +1,53 @@
 package com.br.M1S2.miniprojeto02modulo01.controller;
 import com.br.M1S2.miniprojeto02modulo01.entity.AlunoEntity;
+import com.br.M1S2.miniprojeto02modulo01.entity.ProfessorEntity;
 import com.br.M1S2.miniprojeto02modulo01.service.AlunoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("alunos")
 public class AlunoController {
 
-    private final AlunoService service;
-    public AlunoController(AlunoService service) {
-        this.service = service;
-    }
-
+    private final AlunoService alunoService;
 
     @PostMapping
-    public ResponseEntity<AlunoEntity> post(@RequestBody AlunoEntity newAluno) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.adicionarAluno(newAluno));
+    public ResponseEntity<AlunoEntity> adicionarAluno(@RequestBody AlunoEntity newAluno) {
+        log.debug("Aluno adicionado: {}", newAluno);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoService.adicionarAluno(newAluno));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<AlunoEntity> getId(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.obterAlunoPorId(id));
+    public ResponseEntity<AlunoEntity> obterAlunoPorId(@PathVariable Long id) {
+        log.debug("Aluno encontrado com id: {}", id);
+        return ResponseEntity.status(HttpStatus.OK).body(alunoService.obterAlunoPorId(id));
     }
 
     @GetMapping
-    public ResponseEntity<List<AlunoEntity>> get() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.listarAlunos());
+    public ResponseEntity<List<AlunoEntity>> listarAlunos() {
+        List<AlunoEntity> alunos = alunoService.listarAlunos();
+        log.debug("Total de alunos encontrados: {}", alunos.size());
+        return ResponseEntity.status(HttpStatus.OK).body(alunoService.listarAlunos());
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<AlunoEntity> put(@PathVariable Long id, @RequestBody AlunoEntity entity) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.atualizarAluno(id, entity));
+    public ResponseEntity<AlunoEntity> atualizarAluno(@PathVariable Long id, @RequestBody AlunoEntity entity) {
+        AlunoEntity alunos = alunoService.atualizarAluno(id, entity);
+        log.debug("Aluno atualizado: {}", alunos);
+        return ResponseEntity.status(HttpStatus.OK).body(alunoService.atualizarAluno(id, entity));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> dell(@PathVariable Long id) {
-        service.deletarAluno(id);
+    public ResponseEntity<Void> deletarAluno(@PathVariable Long id) {
+        alunoService.deletarAluno(id);
+        log.debug("Aluno deletado: {}", id);
         return ResponseEntity.noContent().build();
     }
 
