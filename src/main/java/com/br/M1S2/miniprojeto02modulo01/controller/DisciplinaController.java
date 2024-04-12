e3c23dabdba0a62c9895acfb69c6f2b4b82e524f
Ana Helena Peres <110307085+anahperes@users.noreply.github.com>
2024-04-11 21:31:27 -0300

    alterado log disciplina

--- src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/DisciplinaController.java
+++ src/main/java/com/br/M1S2/miniprojeto02modulo01/controller/DisciplinaController.java
@@ -21,40 +21,35 @@ public class DisciplinaController {
 
     @GetMapping
     public ResponseEntity<List<DisciplinaEntity>> listarDisciplinas() {
-        log.info("GET /api/disciplinas -> Listando todas as disciplinas");
         List<DisciplinaEntity> disciplinas = disciplinaService.listarDisciplinas();
-        log.debug("GET /api/disciplinas -> Total de disciplinas encontradas: {}", disciplinas.size());
+        log.debug("Total de disciplinas encontradas: {}", disciplinas.size());
         return ResponseEntity.status(HttpStatus.OK).body(disciplinas);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<DisciplinaEntity> obterDisciplinaPorId(@PathVariable Long id) {
-        log.info("GET /api/disciplinas/{} -> Obtendo disciplina por ID", id);
         DisciplinaEntity disciplina = disciplinaService.obterDisciplinaPorId(id);
-        log.debug("GET /api/disciplinas/{} -> Disciplina encontrada: {}", id, disciplina);
+        log.debug("Disciplina encontrada: {}", id, disciplina);
         return ResponseEntity.status(HttpStatus.OK).body(disciplina);
     }
 
     @PostMapping
     public ResponseEntity<DisciplinaEntity> adicionarDisciplina(@RequestBody DisciplinaEntity disciplina) {
-        log.info("POST /api/disciplinas -> Adicionando nova disciplina: {}", disciplina);
         DisciplinaEntity novaDisciplina = disciplinaService.adicionarDisciplina(disciplina);
-        log.info("POST /api/disciplinas -> Nova disciplina adicionada: {}", novaDisciplina);
+        log.debug("Nova disciplina adicionada: {}", novaDisciplina);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaDisciplina);
     }
     @PutMapping("/{id}")
     public ResponseEntity<DisciplinaEntity> atualizarDisciplina(@PathVariable Long id, @RequestBody DisciplinaEntity disciplinaAtualizada) {
-        log.info("PUT /api/disciplinas/{} -> Atualizando disciplina com o ID: {}", id, id);
         DisciplinaEntity disciplina = disciplinaService.atualizarDisciplina(id, disciplinaAtualizada);
-        log.info("PUT /api/disciplinas/{} -> Disciplina atualizada: {}", id, disciplina);
+        log.debug("Disciplina atualizada: {}", id, disciplina);
         return ResponseEntity.status(HttpStatus.OK).body(disciplina);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarDisciplina(@PathVariable Long id) {
-        log.info("DELETE /api/disciplinas/{} -> Deletando disciplina com o ID: {}", id, id);
         disciplinaService.deletarDisciplina(id);
-        log.info("DELETE /api/disciplinas/{} -> Disciplina deletada com sucesso", id);
+        log.debug("Disciplina deletada com sucesso", id);
         return ResponseEntity.noContent().build();
     }
 }
